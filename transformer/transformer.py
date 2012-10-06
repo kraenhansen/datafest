97132743e799d36a6ae5e3bcf7cb8502d5dd8ba2
Troels Nielsen <bn.troels@gmail.com>
2012-10-06 12:41:35 +0200

    Transforms and keyvalue receiver

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,10 +1,13 @@
 from lxml import etree
+from oaipmh.client import Client
+from oaipmh.metadata import MetadataRegistry, MetadataReader
+from itertools import *
 
-def parse_transformations(txt):
+def parse_transforms(txt):
     errorneous_lines = []
     transforms = []
 
-    for line in txt:
+    for line in txt.split('\n'):
         line = line.strip()
         if not line or line.startswith('#'):
             continue
@@ -14,7 +17,7 @@ def parse_transformations(txt):
                 'reason': 'Missing ->'
             })
             continue
-            
+
         path, field  = (x.strip() for x in line.split('->', 2))
         transforms.append({'path': path, 'field': field})
     return transforms, errorneous_lines
@@ -46,4 +49,41 @@ def read_fields(record, transforms):
                 raise Exception("failed to find: %s" % original_part)
         yield (field, unicode(data))
 
+def fix_transforms(transforms):
+    res = []
+    for transform in transforms:
+        new_transform = transform.copy()
+        if not new_transform['path'].endswith('/'):
+            new_transform['path'] += '/'
+        new_transform['path'] += 'text()'
+        res.append(new_transform)
+    return res
 
+def get_client(url, transforms):
+    transforms = fix_transforms(transforms)
+    registry = MetadataRegistry()
+    c = Client(url, registry)
+    metadata = c.listMetadataFormats()
+    metadata[0] = [
+        'fbb', 'http://www.kulturarv.dk/fbb/fbb.xsd', 'http://www.kulturarv.dk/fbb']
+    namespaces = dict((x[0], x[2]) for x in metadata)
+    fields = dict((transform['field'], ('textList', transform['path']))
+                  for transform in transforms)
+    namespace = metadata[0][0]
+    registry.registerReader(namespace, MetadataReader(fields=fields, namespaces=namespaces))
+    return c, namespace
+
+def list_records(url, transforms):
+    client, ns = get_client(url, transforms)
+    return client.listRecords(metadataPrefix=ns)
+
+def get_record(url, transforms, identifier):
+    client, ns = get_client(url, transforms)
+    return client.getRecord(identifier=identifier, metadataPrefix=ns)
+
+def get_key_values(url, transform_sheet, identifier):
+    transforms, errors = parse_transforms(transform_sheet)
+    if errors:
+        print errors
+    return get_record(url, transforms, identifier)
+    
