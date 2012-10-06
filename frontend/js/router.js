e49d7b4c94ac81d27edc3d55c89591f4e3cd65c8
Jens Christian Hillerup <jens@hillerup.net>
2012-10-06 14:17:28 +0200

    shiny ui

--- frontend/js/router.js
+++ frontend/js/router.js
@@ -5,7 +5,8 @@ define(
             {
                 view: null,
                 routes: {
-                    "dataset/:id": "loadDataset"
+                    "dataset/:id": "loadDataset",
+                    "": "hideDataset"
                 },
                 initialize: function(view) {
                     this.view = view;
@@ -20,10 +21,15 @@ define(
                             model = new Backbone.Model(data);
                             
                             var records = new DataSetCollection(data.records);
-                            
+                            $("#datasets_table").hide();
                             this.view.reset(model);
                         }, this)
                     );
+                },
+                hideDataset: function() {
+                    console.log('hide');
+                    $("#datasets_table").show();
+                    this.view.hide();
                 }
             }
         );
