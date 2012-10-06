5f5376809c86da6da1dd74a3bee40a14492d29a2
Jens Christian Hillerup <jens@hillerup.net>
2012-10-06 10:55:36 +0200

    frontend code, not really data-driven yet

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,25 @@
+require.config({
+    urlArgs: "bust="+(new Date()).getTime(),
+    paths: {
+        text: 'vendor/text'
+    }
+});
 
+require(
+    ['router', 'views/dataset.view'],
+    
+    function(DatafestRouter, DataSetView) {
+        console.log('app initialized');
+
+        // Fire up the app
+        // Do stuff when DOM is ready
+        $(function() {
+            var view = new DataSetView();
+            var router = new DatafestRouter(view);
+            
+            $("body").append(view.$el);
+
+            Backbone.history.start({});
+        });
+    }
+);
