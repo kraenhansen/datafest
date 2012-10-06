c63e274af414c32add20ecbf523eefcd2e821246
Kræn Hansen <post@creen.dk>
2012-10-06 10:48:21 +0200

    Changes to the API

--- datafest/urls.py
+++ datafest/urls.py
@@ -2,14 +2,16 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from datasets.api import DatasetResource
+from datasets.api import DatasetResource, RecordResource
 admin.autodiscover()
 
 dataset_resource = DatasetResource()
+record_resource = RecordResource()
 
 urlpatterns = patterns('',
     url(r'^$', 'datasets.views.overview', name='overview'),
     url(r'^test$', 'datasets.views.test', name='test'),
+    (r'^api/%s/(?P<dataset_pk>\w+)/' % dataset_resource._meta.resource_name, include(record_resource.urls)),
     (r'^api/', include(dataset_resource.urls)),
     # Examples:
     # url(r'^datafest/', include('datafest.foo.urls')),
--- datasets/models.py
+++ datasets/models.py
@@ -5,3 +5,8 @@ class Dataset(models.Model):
 	organisation = models.CharField(max_length=200)
 	contact_name = models.CharField(max_length=200)
 	contact_email = models.EmailField()
+	pmh_url = models.CharField(max_length=2000)
+	metadata_prefix = models.CharField(max_length=200)
+
+class Resource(object):
+	title = ""
